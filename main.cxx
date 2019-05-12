eb283be0fe573cfed7c84d288d53ec197d6fca57
Julien Schueller <schueller@phimeca.com>
2019-05-12 13:33:39 +0200

    cxx_auto_type

--- main.cxx
+++ main.cxx
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[])
 {
   std::vector<int> c { 1,2,3,4,5,6,7 };
-    int x = 5;
+    auto x = 5;
     c.erase(std::remove_if(c.begin(), c.end(), [x](int n) { return n < x; } ), c.end());
  
     std::cout << "c: ";
